6266adc5226fa8c10a1e1bbefe4a20d0d288735b
AlejandroMarques <alejandromgdev@protonmail.com>
2024-12-10 11:25:13 +0100

    type http responses

--- 01-TypeScript-intro/src/bases/03-classes.ts
+++ 01-TypeScript-intro/src/bases/03-classes.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { Move, PokeapiResponse } from '../interfaces/pokeapi-response.interface';
 export class Pokemon {
   
   get imageUrl():string {
@@ -18,8 +19,8 @@ export class Pokemon {
     console.log(`${this.name}, ${this.name}`);
   }
 
-  async getMoves() {
-    const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${this.id}`)
+  async getMoves(): Promise<Move[]> {
+    const { data } = await axios.get<PokeapiResponse>(`https://pokeapi.co/api/v2/pokemon/${this.id}`)
     return data.moves;
   }
 
