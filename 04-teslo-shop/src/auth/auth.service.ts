02f3c5c90f73916890637c604cee0918f4339749
AlejandroMarques <alejandromgdev@protonmail.com>
2024-12-20 18:30:10 +0100

    init auth module

---
+++ 04-teslo-shop/src/auth/auth.service.ts
@@ -0,0 +1,60 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import * as bcrypt from 'bcrypt';
+
+import { CreateUserDto, LoginUserDto } from './dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async register(createUserDto: CreateUserDto) {
+    try {
+      const { password, ...userData } = createUserDto;
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10),
+      });
+
+      await this.userRepository.save(user);
+
+      return user;
+    } catch (error) {
+      this.handleExceptions(error);
+    }
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true },
+    });
+
+    if(!user)
+      throw new UnauthorizedException('Credentials are not valid (email)')
+
+    if(!bcrypt.compareSync(password, user.password)) 
+      throw new UnauthorizedException('Credentials are not valid (password)')
+      
+    return user;
+  }
+
+  private handleExceptions(error: any): never {
+    if (error.code === '2305') throw new BadRequestException(error.detail);
+
+    console.log(error);
+    throw new InternalServerErrorException('Check server logs');
+  }
+}
