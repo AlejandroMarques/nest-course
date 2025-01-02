47eab4b829c9107855d02170cce0e0e7d2f451e3
alex <alejandro.marques.guerrero@gmail.com>
2025-01-02 12:44:59 +0100

    Deleted test endpoints

--- 04-teslo-shop/src/auth/auth.controller.ts
+++ 04-teslo-shop/src/auth/auth.controller.ts
@@ -3,18 +3,12 @@ import {
   Get,
   Post,
   Body,
-  UseGuards,
 } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
-import { GetUser,  RoleProtected, Auth} from './decorators';
+import { GetUser, Auth} from './decorators';
 import { User } from './entities/user.entity';
-import { RawHeaders } from 'src/common/decorators/raw-headers.decorator';
-import { UserRoleGuard } from './guards/user-role.guard';
-import { ValidRoles } from './interfaces';
-
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -27,35 +21,6 @@ export class AuthController {
   @Post('login')
   login(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
-  }
-
-  @Get('private')
-  @UseGuards(AuthGuard())
-  testPrivateRoute(
-    @GetUser() user: User, 
-    @GetUser('email') userEmail: string,
-    @RawHeaders() headers: string[]
-  ) {
-    return { ok: true, message: 'Hola mundo private', user, userEmail, headers };
-  }
-
-  @Get('private2')
-  @RoleProtected(ValidRoles.admin, ValidRoles.superUser)
-  @UseGuards(AuthGuard(), UserRoleGuard)
-  testPrivateRoute2(@GetUser() user: User) {
-    return {
-      ok: true,
-      user
-    }
-  }
-
-  @Get('private3')
-  @Auth()
-  testPrivateRoute3(@GetUser() user: User) {
-    return {
-      ok: true,
-      user
-    }
   }
 
   @Get('check-status')
